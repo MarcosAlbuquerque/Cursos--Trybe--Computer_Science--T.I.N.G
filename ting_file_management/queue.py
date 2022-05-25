7dd757266798a85ff81fcefe31f98c1ce132a746
Marcos Albuquerque <mpdsa20@gmail.com>
2022-05-24 21:29:06 -0300

    Requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,20 @@
 class Queue:
+    queue = []
+
     def __init__(self):
-        print("LEEEEROY JENNNNKINSSSSS")
+        self.queue.clear()
+        self.queue
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        return self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        return self.queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index >= 0 and index < len(self.queue):
+            return self.queue[index]
+        raise IndexError()
