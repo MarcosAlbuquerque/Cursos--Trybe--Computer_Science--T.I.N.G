53dc4b7140fd24f7aba6be664020a639d7a705db
Marcos Albuquerque <mpdsa20@gmail.com>
2022-05-24 22:39:47 -0300

    Requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file) as file:
+            return [line.strip() for line in file]
+    except FileNotFoundError:
+        print("Arquivo %s não encontrado" % (path_file), file=sys.stderr)
+
+
+"""
+[references]
+------------
+https://www.w3schools.com/python/python_ref_string.asp
+https://www.delftstack.com/pt/howto/python/python-print-to-stderr/
+"""
