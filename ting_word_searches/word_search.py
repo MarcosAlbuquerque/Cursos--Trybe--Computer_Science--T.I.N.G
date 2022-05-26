fd0e922a5748c97fabd15ca3133f0cc412edccb6
Marcos Albuquerque <mpdsa20@gmail.com>
2022-05-26 18:39:50 -0300

    Dedico a conclusao deste curso inteiramente ao meu pai

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -14,4 +14,15 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    files_list = instance.queue
+    occurrences = list()
+    result = dict()
+    for file in files_list:
+        lines = file["linhas_do_arquivo"]
+        for index, line in enumerate(lines):
+            if word in line.lower():
+                result["arquivo"] = file["nome_do_arquivo"]
+                result["palavra"] = word
+                occurrences.append({"conteudo": line, "linha": index + 1})
+    result["ocorrencias"] = occurrences
+    return [] if result["ocorrencias"] == [] else [result]
